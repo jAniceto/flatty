e61348f8a86e9b62ef2dec76cabc35696dfd71df
José Aniceto <jose.pedro.aniceto@gmail.com>
2017-01-21 20:10:31 +0000

    Completed CV and other small fixes and modifications

--- js/script.js
+++ js/script.js
@@ -1,36 +1,28 @@
-/*
-Theme: Flatfy Theme
-Author: Andrea Galanti
-Bootstrap Version 
-Build: 1.0
-http://www.andreagalanti.it
-*/
-
-$(window).load(function() { 
-	//Preloader 
-	$('#status').delay(300).fadeOut(); 
+$(window).load(function() {
+	//Preloader
+	$('#status').delay(300).fadeOut();
 	$('#preloader').delay(300).fadeOut('slow');
 	$('body').delay(550).css({'overflow':'visible'});
 })
 
 $(document).ready(function() {
 		//animated logo
-		$(".navbar-brand").hover(function () {
-			$(this).toggleClass("animated shake");
-		});
+		// $(".navbar-brand").hover(function () {
+		// 	$(this).toggleClass("animated shake");
+		// });
 		
 		//animated scroll_arrow
 		$(".img_scroll").hover(function () {
 			$(this).toggleClass("animated infinite bounce");
 		});
-		
+
 		//Wow Animation DISABLE FOR ANIMATION MOBILE/TABLET
 		wow = new WOW(
 		{
 			mobile: false
 		});
 		wow.init();
-		
+
 		//MagnificPopup
 		$('.image-link').magnificPopup({type:'image'});
 
@@ -64,13 +56,12 @@ $(document).ready(function() {
 					}
 			}
 		});
-		
+
 		//Subscribe
 		new UIMorphingButton( document.querySelector( '.morph-button' ) );
 		// for demo purposes only
-		[].slice.call( document.querySelectorAll( 'form button' ) ).forEach( function( bttn ) { 
+		[].slice.call( document.querySelectorAll( 'form button' ) ).forEach( function( bttn ) {
 			bttn.addEventListener( 'click', function( ev ) { ev.preventDefault(); } );
 		} );
 
 });
-
